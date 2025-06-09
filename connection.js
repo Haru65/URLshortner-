acd5f57f3070b3d2013899c98e8c888d8c653cdd
unknown <adityakhandagale69@gmail.com>
2025-06-09 23:50:03 +0530

    added gcloud yaml

--- connection.js
+++ connection.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const url = require("./models/url");
-const MONGO_URI = process.env.MONGO_URI || "mongo://mongodb:27017/shortUrl";
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGO_URL || "mongodb+srv://adityakhandagale69:KR7Sm5bmUbgRMUwI@cluster0.0pq59kn.mongodb.net/";
 async function Connection(url ){
-    return  mongoose.connect(MONGO_URI)
+    return  mongoose.connect(MONGO_URL)
 }
 module.exports = {
     Connection

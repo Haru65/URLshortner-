e0824b29dbf7e31fca71709136221e0d84ac1f16
unknown <adityakhandagale69@gmail.com>
2025-06-10 00:45:17 +0530

    finally everything

--- connection.js
+++ connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const url = require("./models/url");
-const MONGO_URI = process.env.MONGO_URI || "mongo://mongodb:27017/shortUrl";
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://adityakhandagale69:KR7Sm5bmUbgRMUwI@cluster0.0pq59kn.mongodb.net/";
 async function Connection(url ){
     return  mongoose.connect(MONGO_URI)
 }
--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const url = require("./models/url")
 const path = require ("path")
 const router = express.Router();
 const app = express();
-const port = 8080;
-const MONGO_URI = process.env.MONGO_URI || "mongodb://mongodb:27017/shortUrl";
+const port = 8000;
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://adityakhandagale69:KR7Sm5bmUbgRMUwI@cluster0.0pq59kn.mongodb.net/";
 //middleware
 app.use(express.json( ))
 app.use(express.urlencoded({ extended: true }));
